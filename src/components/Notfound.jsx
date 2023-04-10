2ad245d1b1293d07332b0f48db4d2c76d2bb7956
srinuVasuDev Bali <111112374+BSRINU1996@users.noreply.github.com>
2023-04-10 16:21:14 +0530

    Update Notfound.jsx

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Notfound = () => {
     return (
         <>
-            <h1>This is the NotFound Component. </h1>
+            <h1>This is the NotFound Component.if not provided an correct existing path , this component will dispaly </h1>
         </>
     )
 }
